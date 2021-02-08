4533bc5486925c0a627b0131699068e09f2acdf0
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-02-08 12:18:24 +0000

    inst/

---
+++ inst/scripts/utils/st.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+for f in .gitignore .Rbuildignore .Rinstignore .travis.yml \
+         data/ DESCRIPTION docs/ INDEX index.md inst/ LICENSE LICENSE.md man/ NAMESPACE NEWS.md pkgdown/ R/ st.sh vignettes/
+do
+  git add ${f}
+  git commit -m "${f}"
+done
+
+git push
