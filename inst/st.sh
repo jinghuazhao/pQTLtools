a27b69ad5e2198ce99e0c2d9004e8e2cd565df90
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-02-08 13:52:29 +0000

    inst/

--- inst/st.sh
+++ inst/st.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 for f in .github .gitignore .Rbuildignore .Rinstignore .travis.yml \
-         data/ DESCRIPTION docs/ INDEX index.md inst/ LICENSE LICENSE.md man/ NAMESPACE NEWS.md pkgdown/ R/ st.sh vignettes/
+         data/ DESCRIPTION docs/ INDEX index.md inst/ LICENSE LICENSE.md man/ NAMESPACE NEWS.md pkgdown/ R/ st.sh README.md vignettes/
 do
   echo adding ${f}
   git add -f ${f}
