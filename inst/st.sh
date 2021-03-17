1d1c010126e08a7dfad700adc201c9250e5d3797
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-03-17 21:05:56 +0000

    inst/

--- inst/st.sh
+++ inst/st.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-Rscript -e "library(pkgdown);build_articles();build_news();build_reference()"
+Rscript -e "library(pkgdown);build_home();build_articles();build_news();build_reference()"
 
 for f in .github .gitignore .Rbuildignore .Rinstignore .travis.yml \
          data/ DESCRIPTION docs/ INDEX inst/ LICENSE LICENSE.md man/ NAMESPACE NEWS.md pkgdown/ R/ README.md vignettes/
