c8d365ecc8daf72bf780fff5f2aa49ce0857502a
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-03-17 20:46:43 +0000

    inst/

--- inst/st.sh
+++ inst/st.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-Rscript -e "library(pkgdown);build_articles()"
+Rscript -e "library(pkgdown);build_articles();build_news();build_reference()"
 
 for f in .github .gitignore .Rbuildignore .Rinstignore .travis.yml \
-         data/ DESCRIPTION docs/ INDEX inst/ LICENSE LICENSE.md man/ NAMESPACE NEWS.md pkgdown/ R/ st.sh README.md vignettes/
+         data/ DESCRIPTION docs/ INDEX inst/ LICENSE LICENSE.md man/ NAMESPACE NEWS.md pkgdown/ R/ README.md vignettes/
 do
   echo adding ${f}
   git add -f ${f}
