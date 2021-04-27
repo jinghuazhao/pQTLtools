1882cb779f07af7726179b82b9fe554e9e835bfb
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-04-27 21:49:54 +0100

    R/

--- R/utils.R
+++ R/utils.R
@@ -206,11 +206,12 @@ swap <- function(x,y)
 
 blank_plot <- function(message)
 {
-        requireNamespace("ggplot2", quietly=TRUE)
-        ggplot2::ggplot(data.frame(a=0,b=0,n=message)) +
-        ggplot2::geom_text(ggplot2::aes(x=a,y=b,label=n)) +
-        ggplot2::labs(x=NULL,y=NULL) +
-        ggplot2::theme(axis.text=ggplot2::element_blank(), axis.ticks=ggplot2::element_blank())
+   requireNamespace("ggplot2", quietly=TRUE)
+   a <- b <- NA
+   ggplot2::ggplot(data.frame(a=0,b=0,n=message)) +
+   ggplot2::geom_text(ggplot2::aes(x=a,y=b,label=n)) +
+   ggplot2::labs(x=NULL,y=NULL) +
+   ggplot2::theme(axis.text=ggplot2::element_blank(), axis.ticks=ggplot2::element_blank())
 }
 
 mr_scatter_plot2 <- function (mr_results, dat, alpha=0.05)
