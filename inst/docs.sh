6ee3b6adb709c412ec13e5c02a99e5e69a395498
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-05-27 17:01:50 +0100

    inst/

--- inst/docs.sh
+++ inst/docs.sh
@@ -25,6 +25,9 @@ function build()
      rm -rf docs/articles/pQTLtools
      mv vignettes/pQTLtools docs/articles/
   fi
+  if [ -f vignettes/fig.html ]; then
+     mv vignettes/fig.html docs/articles/
+  fi
 # add entry for reference to pkgdown/_pkgdown.yml  
 }
 
