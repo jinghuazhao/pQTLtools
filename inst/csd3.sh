ba5d5fc414fbfee03a10bdc3a5b1f3e633fc807d
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-06-09 15:52:09 +0100

    inst/

--- inst/csd3.sh
+++ inst/csd3.sh
@@ -8,8 +8,5 @@ R CMD build --resave-data --compact-vignettes pQTLtools
 R CMD check --as-cran pQTLtools_0.1.tar.gz
 R CMD INSTALL pQTLtools_0.1.tar.gz
 
-# also work:
-# R CMD INSTALL pQTLtools
-
-rm -rf pQTLtools/pQTLtools.Rcheck
-mv pQTLtools_0.1.tar.gz R
+rm -rf ${HOME}/R/pQTLtools.Rcheck
+mv pQTLtools_0.1.tar.gz pQTLtools.Rcheck R
