3fbf2326f3ea7108f544d2f4f3666ee07c9a4602
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2022-08-01 23:08:37 +0100

    inst/

--- inst/snakemake/workflow/r/MR_functions.R
+++ inst/snakemake/workflow/r/MR_functions.R
@@ -101,9 +101,9 @@ MR_PCA <- function(df_mr, ld, harmonise=FALSE, var_exp=0.99)
     Omega <- se.y %o% se.y * ld
     pcOmega <- t(PCA$rotation[,1:K])%*%Omega%*%PCA$rotation[,1:K]
     ipcOmega <- solve(pcOmega)
-    beta_IVWcorrel.pc <- solve(t(betaXG0)%*%ipcOmega%*%betaXG0)*t(betaXG0)%*%ipcOmega%*%betaYG0
-    beta_IVWcorrel.pc <- as.numeric(beta_IVWcorrel.pc)
-    se_IVWcorrel.fixed.pc <- sqrt(solve(t(betaXG0)%*%ipcOmega%*%betaXG0)) %>% as.numeric()
+    ibetaXG0ipcOmegabetaXG0 <- solve(t(betaXG0)%*%ipcOmega%*%betaXG0)
+    beta_IVWcorrel.pc <- as.numeric(ibetaXG0ipcOmegabetaXG0*t(betaXG0)%*%ipcOmega%*%betaYG0)
+    se_IVWcorrel.fixed.pc <- as.numeric(sqrt(ibetaXG0ipcOmegabetaXG0))
     Z_score <- beta_IVWcorrel.pc / se_IVWcorrel.fixed.pc
     P_value <- 2*pnorm(-abs(Z_score))
     LCI <- beta_IVWcorrel.pc - qnorm(0.975)*se_IVWcorrel.fixed.pc
