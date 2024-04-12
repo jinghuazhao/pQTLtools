da57eb5fe16e29c60fc86e92019fe3fc68501914
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-04-12 12:54:53 +0100

    R/

--- R/utils.R
+++ R/utils.R
@@ -454,7 +454,7 @@ import_eQTLCatalogue <- function(ftp_path, region, selected_gene_id, column_name
 
 #' Colocalisation analysis
 #'
-#' The function takes eQTL and GWAS summary statistics for a colocalisation analysis..
+#' The function takes eQTL and GWAS summary statistics for a colocalisation analysis.
 #'
 #' @param eqtl_sumstats eQTL summary data.
 #' @param gwas_sumstats GWAS summary data.
@@ -953,8 +953,8 @@ mr_leaveoneout_plot2 <- function (leaveoneout_results, alpha = 0.05)
 #' This function takes data intrumental variables as produced by `format_data()` and
 #' used to perform MR analysis against a list of outcomes from MR-Base.
 #'
-#' @param ivs Instrumental variables from format_data().
-#' @param ids A list of MR-Base IDs.
+#' @param ivs Instrumental variables from `format_data()`.
+#' @param outcome The counterpart for outcome.
 #' @param mr_plot to produce plots.
 #' @param prefix a prefix for output files.
 #' @param reverse if TRUE, perform reverse MR.
@@ -974,25 +974,29 @@ mr_leaveoneout_plot2 <- function (leaveoneout_results, alpha = 0.05)
 #' @examples
 #' library(TwoSampleMR)
 #' library(pQTLtools)
-#' # Original examples
-#' f <- file.path(find.package("pQTLtools",lib.loc=.libPaths()),"tests","Ins.csv")
-#' ivs <- format_data(read.csv(f))
-#' ids <- c("ieu-a-7","ebi-a-GCST007432")
-#' pqtlMR(ivs, ids, mr_plot=FALSE)
-#' # A bidirectional analysis
-#' f <- file.path(find.package("pQTLtools",lib.loc=.libPaths()),"tests","ms.ins")
-#' ivs <- format_data(read.table(f, header=TRUE), samplesize_col="N")
-#' ids <- "ieu-b-18"
-#' # MR
-#' pqtlMR(ivs, ids, prefix="MS-")
-#' # reverse MR
-#' pqtlMR(ivs, ids, ,prefix="MS_rev-",reverse=TRUE)
+#' fi <- file.path(find.package("pQTLtools",lib.loc=.libPaths()),"tests","Ins.csv")
+#' ivs <- format_data(read.csv(fi),type="exposure")
+#' fo <- file.path(find.package("pQTLtools",lib.loc=.libPaths()),"tests","Out.csv")
+#' outcome <- format_data(read.csv(fo),type="outcome")
+#' pqtlMR(ivs, outcome, prefix="IL6R-")
+#' pqtlMR(ivs, outcome, prefix="IL6R_rev-",reverse=TRUE)
+#' # Phenotype,SNP,effect_allele,other_allele,eaf,beta,se,pval
+#' # ABO,rs505922,C,T,0.313,1.298,0.014,1.2e-1828
+#' # LIFR,rs635634,T,C,0.180,-0.300,0.032,6.00E-21
+#' # f <- file.path(find.package("pQTLtools",lib.loc=.libPaths()),"tests","ms.ins")
+#' # ivs <- format_data(read.table(f, header=TRUE), samplesize_col="N")
+#' # SNP Phenotype effect_allele other_allele eaf beta se pval N
+#' # rs1800693 TNFB T C 0.6033 0.0282  0.0136 0.0389045   11787
+#' # rs2364485 TNFB A C 0.1645 6514963 0.1759 1.62181e-20 11344
+#' # ids <- c("ieu-a-7","ebi-a-GCST007432")
+#' # outcome_data <- TwoSampleMR::extract_outcome_data(snps=with(ivs,SNP),outcomes=ids)
+#' # https://raw.githubusercontent.com/MRCIEU/epigraphdb-pqtl/master/scripts/MR-pQTL-script.R
 #'
 #' @note
 #' Adapted from script by Jie Zheng.
 #' @keywords utilities
 
-pqtlMR <- function(ivs, ids, mr_plot=FALSE, prefix="pQTL-combined-", reverse=FALSE)
+pqtlMR <- function(ivs, outcome, mr_plot=FALSE, prefix="pQTL-combined-", reverse=FALSE)
 {
    for (p in c("TwoSampleMR")) {
      if (length(grep(paste("^package:", p, "$", sep=""), search())) == 0) {
@@ -1000,8 +1004,6 @@ pqtlMR <- function(ivs, ids, mr_plot=FALSE, prefix="pQTL-combined-", reverse=FAL
             warning(paste("This function needs package `", p, "' to be fully functional; please install", sep=""))
      }
    }
-   outcome_data <- TwoSampleMR::extract_outcome_data(snps=with(ivs,SNP),outcomes=ids)
-   harmonise <- TwoSampleMR::harmonise_data(ivs,outcome_data)
    id.exposure <- NA
    id.outcome <- NA
    effect_allele.exposure <- NA
@@ -1018,6 +1020,7 @@ pqtlMR <- function(ivs, ids, mr_plot=FALSE, prefix="pQTL-combined-", reverse=FAL
    pval.exposure <- NA
    pval.outcome <- NA
    swap_unique_var_a <- NA
+   harmonise <- TwoSampleMR::harmonise_data(ivs,outcome)
    if (reverse) harmonise <- subset(within(harmonise,
    {
       swap(id.exposure,id.outcome)
@@ -1031,9 +1034,7 @@ pqtlMR <- function(ivs, ids, mr_plot=FALSE, prefix="pQTL-combined-", reverse=FAL
       swap(pval.exposure,pval.outcome)
     }),select=-swap_unique_var_a)
    result <- single <- NULL
-#  main MR analysis
    try(result <- TwoSampleMR::mr(harmonise,method_list=c("mr_wald_ratio", "mr_ivw")))
-#  single SNP MR using Wald ratio
    try(single <- TwoSampleMR::mr_singlesnp(harmonise))
    invisible(lapply(c("harmonise","result","single"), function(x) if (exists(x))
              write.table(format(get(x),digits=3),file=paste0(prefix,x,".txt"),
