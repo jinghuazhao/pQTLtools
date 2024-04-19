0349dfc821b0d1052a857dc9279a1787c22a2ff0
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-04-19 22:00:35 +0100

    --no-verify

--- inst/csd3.sh
+++ inst/csd3.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/bash
 
-module load gcc/6 texlive ceuadmin/R
+if [ "$(uname -n | sed 's/-[0-9]*$//')" == "login-q" ]; then
+   module load ceuadmin/libssh/0.10.6-icelake
+   module load ceuadmin/openssh/9.7p1-icelake
+   module load bcftools/1.13/gcc/intel-oneapi-mkl/vi7yswnv
+   module load ceuadmin/R/4.3.3-icelake
+else
+   module load gcc/6 ceuadmin/R
+fi
+module load texlive
 
 export version=$(awk '/Version/{print $2}' ~/pQTLtools/DESCRIPTION)
 R CMD build --resave-data --compact-vignettes=both pQTLtools
