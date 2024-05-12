713dead8f63c6aab4fadfe92f436f643af8981a3
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-05-12 16:42:14 +0100

    --no-verify

---
+++ R/novelty_check.R
@@ -0,0 +1,122 @@
+#' Locus novelty check
+#'
+#' This function checks novelty of a list of loci such as pQTLs against a published list.
+#' Both known_loci and query_loci have these variables: chr, pos, uniprot, rsid, prot.
+#'
+#' @param known_loci A data.frame of published loci.
+#' @param query_loci A data.frame of loci whose novelties are unclear.
+#' @param ldops arguments for ieugwasr::ld_matrix_local()
+#' @param flanking A flanking distance.
+#' @param pop The reference population as for ieugwasr::ld_matrix().
+#' @param verbose A flag to show nonexistent variants.
+#'
+#' @return A data.frame containing nonnovel loci.
+#' @export
+#' @examples
+#' \dontrun{
+#' options(width=2000)
+#' suppressMessages(require(dplyr))
+#' # SCALLOP-INF list
+#' METAL <- read.delim(file.path(find.package("pQTLtools"),"tests","INF1.METAL")) %>%
+#'          left_join(gap.datasets::inf1[c("prot","gene")]) %>%
+#'          mutate(prot=gene,prot_rsid=paste0(uniprot,"-",rsid),chr=Chromosome,pos=Position)
+#' # UKB_PPP list
+#' require(openxlsx)
+#' results <- "/rds/project/jmmh2/rds-jmmh2-results/public/proteomics"
+#' url <- file.path(results,"UKB-PPP","doc","sun22.xlsx")
+#' ST10 <- read.xlsx(url,"ST10",startRow=3) %>%
+#'         mutate(uniprot=Target.UniProt,rsid=rsID,prot=Assay.Target) %>%
+#'         mutate(prot_rsid=paste0(uniprot,"-",rsid))
+#' sentinels <- left_join(METAL,ST10,by="prot_rsid") %>%
+#'              select(prot_rsid,cis.trans,rsID) %>%
+#'              filter(!is.na(rsID))
+#' inf1 <- c(with(gap.datasets::inf1,uniprot),with(METAL,uniprot)) %>%
+#'         unique()
+#' overlap <- filter(ST10,uniprot %in% inf1)
+#' dim(overlap)
+#' UKB_PPP <- mutate(overlap,
+#'            chrpos=strsplit(overlap[["Variant.ID.(CHROM:GENPOS.(hg37):A0:A1:imp:v1)"]],":"),
+#'            chr=as.integer(unlist(lapply(chrpos,"[[",1))),
+#'            pos=as.integer(unlist(lapply(chrpos,"[[",2))),
+#'            chrpos=paste(chr,pos,sep=":"))
+#' suppressMessages(require(GenomicRanges))
+#' b <- novelty_check(UKB_PPP,METAL)
+#' replication <- filter(b,r2>=0.8)
+#' INF <- "/rds/project/jmmh2/rds-jmmh2-projects/olink_proteomics/scallop/INF/"
+#' # write.table(replication,file=file.path(INF,"work","UKB-PPP.txt"),
+#' #             row.names=FALSE,quote=FALSE,sep="\t")
+#' replication <- read.delim(file.path(find.package("pQTLtools"),"tests","UKB-PPP.txt")) %>%
+#'                select(known.seqnames,known.rsid,query.rsid,query.prot)
+#' variant_list <- unique(c(pull(replication,known.rsid),pull(replication,query.rsid)))
+#' load(file.path(find.package("pQTLtools"),"tests","novel_data.rda"))
+#' prot_rsid <- with(novel_data,paste0(prot,"-",rsid))
+#' prot_rsid_repl <- with(replication,paste0(query.prot,"-",query.rsid))
+#' left <- setdiff(prot_rsid,prot_rsid_repl)
+#' # local LD reference panel by chromosome
+#' # r2 <- LDlinkR::LDmatrix(variant_list,pop="CEU",token=Sys.getenv("LDLINK_TOKEN"))
+#' plink <- "/rds/user/jhz22/hpc-work/bin/plink"
+#' b <- list()
+#' for(i in unique(pull(METAL,Chromosome)))
+#' {
+#'    u <- filter(UKB_PPP,chr %in% i) %>%
+#'         select(chr,pos,uniprot,rsid,prot)
+#'    m <- filter(METAL,Chromosome %in% i) %>%
+#'         select(chr,pos,uniprot,rsid,prot)
+#'    bfile <- file.path(INF,"INTERVAL","per_chr",paste0("chr",i))
+#'    b[[i]] <- novelty_check(u,m,ldops=list(bfile,plink))
+#' }
+#' replication2 <- filter(bind_rows(b), r2>=0.8)
+#' prot_rsid <- with(novel_data %>%
+#'              left_join(gap.datasets::inf1[c("prot","gene")]),paste0(gene,"-",rsid))
+#' prot_rsid_repl <- with(replication2,paste0(query.prot,"-",query.rsid))
+#' novel <- setdiff(prot_rsid,prot_rsid_repl)
+#' }
+
+novelty_check <- function(known_loci,query_loci,ldops=NULL,flanking=1e6,pop="EUR",verbose=TRUE)
+{
+  rsid <- seqnames <- start <- strand <- width <- NA
+  query <- with(known_loci,GenomicRanges::GRanges(seqnames=chr,IRanges::IRanges(start=pos,width=1),
+                                                  uniprot=uniprot,rsid=rsid,pos=pos,prot=prot))
+  subject <- with(query_loci,GenomicRanges::GRanges(seqnames=chr,IRanges::IRanges(start=pos-flanking,end=pos+flanking),
+                                                    uniprot=uniprot,rsid=rsid,pos=pos,prot=prot))
+  fo <- GenomicRanges::findOverlaps(query,subject) %>%
+        data.frame()
+  eq <- subject[fo$subjectHits,]$uniprot==query[fo$queryHits,]$uniprot
+  ov <- data.frame(fo[eq,])
+  ov1 <- query[ov$queryHits,] %>%
+         data.frame() %>%
+         select(-strand,-width) %>%
+         mutate(rsid=if_else(rsid=="-",paste0("chr",seqnames,":",start),rsid))
+  ov2 <- subject[ov$subjectHits,] %>%
+         data.frame() %>%
+         select(-strand,-width)
+  b <- bind_cols(data.frame(ov1) %>% setNames(paste("known",names(ov1),sep=".")),
+                 data.frame(ov2) %>% setNames(paste("query",names(ov2),sep=".")))
+  variant_list <- unique(c(b[["known.rsid"]],b[["query.rsid"]]))
+  if (!is.null(ldops))
+  {
+    r <- ieugwasr::ld_matrix_local(c(b[["known.rsid"]],b[["query.rsid"]]),
+                                   with_alleles=FALSE,
+                                   bfile=bfile,
+                                   plink_bin=plink)
+  } else r <- ieugwasr::ld_matrix(variant_list,pop=pop,with_alleles=FALSE)
+  failure <- setdiff(variant_list,colnames(r))
+  if (verbose) {
+     cat("\nLD information cannot be retrieved for", length(failure), "variants:\n")
+     cat(failure,sep="\n")
+  }
+  known.keep <- intersect(b[["known.rsid"]],colnames(r))
+  query.keep <- intersect(b[["query.rsid"]],colnames(r))
+  ll <- table(b[["known.rsid"]],b[["query.rsid"]])
+  ll[,] <- NA
+  ll[known.keep,query.keep] <- r[known.keep,query.keep]
+  r2 <- sapply(1:nrow(b), function(x) with(b[x, ], ifelse(known.rsid == query.rsid, 1, ll[known.rsid, query.rsid]^2)))
+  invisible(mutate(b,r2=r2))
+}
+
+# l <- matrix(NA,length(b[["known.rsid"]]),length(b[["query.rsid"]]),dimnames=list(b[["known.rsid"]], b[["query.rsid"]]))
+# l[known.keep,query.keep] <- r[known.keep,query.keep]
+# r2 <-  sapply(1:nrow(b),function(x) with(b[x,],ifelse(known.rsid==query.rsid,1,l[known.rsid,query.rsid]^2)))
+
+# wget https://www.biorxiv.org/content/biorxiv/early/2022/06/18/2022.06.17.496443/DC2/embed/media-2.xlsx -O sun22.xlsx
+
---
+++ R/qtl_lookup.R
@@ -0,0 +1,125 @@
+#' QTL lookup
+#'
+#' This function takes MR results (involving pQTL/QTL) to look up QTLs in trait GWASs given a P value and
+#' linkage disequilibrium (LD) cutoffs.
+#' The rationale is that a pQTL may not necessarily be in strong LD with QTL but some other independent signal
+#' in the same region. The interest then lands on association signals below a given P value (p_threshold) and above
+#' a given LD (r2_threshold) thresholds.
+#'
+#' @param d directory where `dat` (below) is held.
+#' @param dat MR results (`protein`, `id`, `pqtl`,`p`, `qtl`, `p_qtl`) whose `proxy`, `p_proxy` and `rsq` variables will be updated.
+#' @param panel reference panel.
+#' @param p_threshold cutoff of QTL association from trait GWASs.
+#' @param r2_threshold cutoff of r^2.
+#' @param pop reference population from 1000Genomes for LD calculation.
+#' @param plink_bin PLINK executable file whose binary files are indicated in `bfile` variable of `dat`.
+#' @param r when specified, the LD(r) is output.
+#' @param r2 when specified, the LD(r^2) is output.
+#' @param xlsx a non-null specification indicates name of an output Excel workbook.
+#' 
+#' @return A data.frame containing the looked up loci.
+#' @export
+#' @examples
+#' \dontrun{
+#' options(width=200)
+#' INF <- Sys.getenv("INF")
+#' suppressMessages(library(dplyr))
+#' d <- file.path(INF,"mr","gsmr","trait")
+#' inf1 <- select(gap.datasets::inf1,prot,target.short)
+#' gsmr_efo <- read.delim(file.path(INF,"mr","gsmr","gsmr-efo.txt")) %>%
+#'             left_join(inf1,by=c('protein'='target.short')) %>%
+#'             mutate(file_gwas=paste(prot,id,"rsid.txt",sep="-"),
+#'                    bfile=file.path(INF,"INTERVAL","per_chr",
+#'                                    paste0("interval.imputed.olink.chr_",chr)),
+#'                    proxy=NA,p_proxy=NA,rsq=NA)
+#' proxies <- qtl_lookup(d,gsmr_efo,plink_bin="/rds/user/jhz22/hpc-work/bin/plink",
+#'                       xlsx=file.path(INF,"mr","gsmr","r2_INTERVAL.xlsx")) %>%
+#'            select(protein,id,Disease,fdr,pqtl,p,qtl,p_qtl,proxy,p_proxy,rsq)
+#' write.table(proxies,file=file.path(INF,"mr","gsmr","r2_INTERVAL.tsv"),
+#'             row.names=FALSE,quote=FALSE,sep="\t")
+#' }
+
+qtl_lookup <- function(d,dat,panel="1000Genomes",p_threshold=1e-3,r2_threshold=0.8,pop="EUR",
+                       plink_bin=NULL,r=NULL,r2=NULL,xlsx=NULL)
+{
+  protein <- p <- SNP <- prot <- Disease <- protein <- fdr <- qtl <- p_qtl <- rsq <- NULL
+  for(i in 1:nrow(dat))
+  {
+     z <- dplyr::slice(dat,i)
+     pqtl <- z[["pqtl"]]
+     cat(z[["file_gwas"]],"\n")
+     gwas <- read.table(file.path(d,basename(z[["file_gwas"]])),header=TRUE) %>%
+             dplyr::arrange(p)
+     h <-  dplyr::filter(gwas,p<=p_threshold)
+     panel_snps <- c(pqtl,dplyr::pull(h,SNP))
+     if (panel=="1000Genomes")
+     {
+        if (length(panel_snps)>500) stop("too many SNPs -- put a more stringent ptreshold")
+        xx <- ieugwasr::ld_matrix(panel_snps,pop="EUR")
+     } else
+     xx <- ieugwasr::ld_matrix(panel_snps,with_alleles=TRUE,pop=pop,bfile=z[["bfile"]],plink_bin=plink_bin)
+     cn <- colnames(xx)
+     inside <- pqtl==gsub("_[A-Z]*","",cn)
+     nn <- c(cn[inside],cn[!inside])
+     if (length(r)==1) r_mat <- xx else r_mat <- xx[nn,nn]
+     if(!is.null(r)) write.table(r_mat,file=paste(prot,id,Disease,pqtl,"r.txt",sep="-"))
+     r2_mat <- r_mat^2
+     if(!is.null(r2)) write.table(r2_mat,file=paste(prot,id,Disease,pqtl,"r2.txt",sep="-"))
+     colnames(r2_mat) <- gsub("_[A-Z]*","",colnames(r2_mat))
+     rownames(r2_mat) <- gsub("_[A-Z]*","",rownames(r2_mat))
+     snps <- intersect(pull(h,SNP),colnames(r2_mat))
+     success <- 1
+     if (length(snps)<1) next else if (length(snps)==1) dat[i,c("proxy","p_proxy","rsq")] <- c(z[c("qtl","p_qtl")],1)
+     else while(length(snps)>1)
+     {
+       success <- -1
+       proxy <- snps[1]
+       snps <- setdiff(snps,proxy)
+       r2_i <- r2_mat[z[["pqtl"]],proxy]
+       p_proxy <- filter(gwas,SNP==proxy) %>%
+                  slice(which.min(p)) %>%
+                  pull(p)
+       cat("Same locus",i,z[["protein"]],z[["id"]],z[["Disease"]],z[["pqtl"]],z[["qtl"]],proxy,r2_i,z[["p_qtl"]],"\n",sep="\t")
+       if(!is.null(r2_i)&!is.na(r2_i)) if(r2_i>r2_threshold) {success <-1; break}
+     }
+     if (success==-1)  while(length(snps)>1)
+     {
+       success <- -1
+       proxy <- snps[1]
+       snps <- setdiff(snps,proxy)
+       r2_i <- r2_mat[z[["pqtl"]],proxy]
+       p_proxy <- filter(gwas,SNP==proxy) %>%
+                  slice(which.min(p)) %>%
+                  pull(p)
+       cat("Independent locus",i,z[["protein"]],z[["id"]],z[["Disease"]],z[["pqtl"]],z[["qtl"]],proxy,r2_i,z[["p_qtl"]],"\n",sep="\t")
+       if(!is.null(r2_i)&!is.na(r2_i)) if(r2_i<r2_threshold) {success <-1; break}
+     }
+     if (success==1)
+     {
+       dat[i,"proxy"] <- proxy
+       dat[i,"p_proxy"] <- p_proxy
+       dat[i,"rsq"] <- r2_i
+     }
+  }
+  if (!is.null(xlsx))
+  {
+    wb <- openxlsx::createWorkbook(xlsx)
+    hs <- openxlsx::createStyle(textDecoration="BOLD", fontColour="#FFFFFF", fontSize=12, fontName="Arial Narrow", fgFill="#4F80BD")
+    proxies <- dplyr::select(dat,protein,id,Disease,fdr,pqtl,p,qtl,p_qtl,proxy,p_proxy,rsq)
+    for (sheet in "proxies")
+    {
+      openxlsx::addWorksheet(wb,sheet,zoom=150)
+      openxlsx::writeData(wb,sheet,sheet,xy=c(1,1),headerStyle=openxlsx::createStyle(textDecoration="BOLD",
+                          fontColour="#FFFFFF", fontSize=14, fontName="Arial Narrow", fgFill="#4F80BD"))
+      body <- get(sheet)
+      openxlsx::writeDataTable(wb, sheet, body, xy=c(1,2), headerStyle=hs, firstColumn=TRUE, tableStyle="TableStyleMedium2")
+      openxlsx::freezePane(wb, sheet, firstActiveCol=3, firstActiveRow=3)
+      width_vec <- apply(body, 2, function(x) max(nchar(as.character(x))+2, na.rm=TRUE))
+    # width_vec_header <- nchar(colnames(body))+2
+      openxlsx::setColWidths(wb, sheet, cols = 1:ncol(body), widths = width_vec)
+      openxlsx::writeData(wb, sheet, tail(body,1), xy=c(1, nrow(body)+2), colNames=FALSE, borders="rows", borderStyle="thick")
+    }
+    openxlsx::saveWorkbook(wb, file=xlsx, overwrite=TRUE)
+  }
+  dat
+}
