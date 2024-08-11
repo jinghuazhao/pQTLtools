cfa07d8000db0fa217bfe81517768e0aa7150421
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-08-11 10:48:39 +0100

    --no-verify

--- inst/csd3.sh
+++ inst/csd3.sh
@@ -4,10 +4,9 @@ if [ "$(uname -n | sed 's/-[0-9]*$//')" == "login-q" ]; then
    module load ceuadmin/libssh/0.10.6-icelake
    module load ceuadmin/openssh/9.7p1-icelake
    module load bcftools/1.13/gcc/intel-oneapi-mkl/vi7yswnv
-   module load ceuadmin/R/4.4.0-icelake
-else
-   module load ceuadmin/R
 fi
+module load ceuadmin/R
+module load mono/5.0.1.1
 module load texlive
 
 export version=$(awk '/Version/{print $2}' ~/pQTLtools/DESCRIPTION)
