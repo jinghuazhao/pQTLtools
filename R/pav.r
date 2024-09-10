c16fb7eaa649e80636430b4b53ab88e4af97a6ef
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-09-10 08:20:08 +0100

    --no-verify

--- R/pav.r
+++ R/pav.r
@@ -1,6 +1,8 @@
-#' @title Protein-Altering Variants
+#' @title Protein-altering variants
 #' @description Variants which directly affect the coding sequence or the protein produced.
-#' @format A vector containing lengths of chromosomes.
+#' @format A vector of consequences..
 #' @rdname protein_altering_variants
+#' @examples
+#' protein_altering_variants
 
 "protein_altering_variants"
