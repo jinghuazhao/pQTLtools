cd53c552f878cc8426979c35cd17a6801a001a89
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2025-01-10 17:07:07 +0000

    inst/

--- inst/docs.sh
+++ inst/docs.sh
@@ -18,7 +18,7 @@ Rscript -e '
 # pandoc README.md --citeproc --mathjax -s --self-contained -o index.html
 }
 
-cp vignettes/ACE.htm vignettes/lz.htm docs/articles
+cp vignettes/ACE.html vignettes/lz.html docs/articles
 if [ -d docs/articles/data ]; then rm -rf docs/articles/data; cp -r vignettes/data docs/articles; fi
 for d in bioconductor esse pQTLtools spectrum
 do
