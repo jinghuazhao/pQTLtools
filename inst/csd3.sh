efbe8ee7296615021d7529c301caf96b9e657ea7
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2025-01-17 11:22:46 +0000

    inst/

--- inst/csd3.sh
+++ inst/csd3.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/bash
 
-if [ "$(uname -n | sed 's/-[0-9]*$//')" == "login-q" ]; then
-   module load ceuadmin/libssh/0.10.6-icelake
-   module load ceuadmin/openssh/9.7p1-icelake
-   module load bcftools/1.13/gcc/intel-oneapi-mkl/vi7yswnv
-fi
 module load ceuadmin/R
 module load mono/5.0.1.1
 module load texlive
